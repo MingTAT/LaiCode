6fca900b52df8afbe2cbea6c4b584592c677893e
MingTAT <314725486@qq.com>
2016-11-25 13:55:51 -0600

    Lecture2

--- laiCodeClass1/src/Lesson2/kClosest.java
+++ laiCodeClass1/src/Lesson2/kClosest.java
@@ -1,33 +1,26 @@
 package Lesson2;
 
+import java.util.Arrays;
+
 public class kClosest {
 
+	
+	public static void main(String[] args){
+		int[] a = {4,7,12,33,100,200};
+		int[] res = kClosest(a,50,3);
+		System.out.println(Arrays.toString(res));
+	}
 	//given a target integer T, a non-negative integer K and an integer 
 	//array A sorted in ascending order, find the K closest numbers to T in A
 	public static int[] kClosest(int[] array, int target, int k){
 
-		int left = 0;
+		int left = 0, index = 0;
 		int right = array.length - 1;
-		
 		int res[] = new int[k];
-		for(int i = 0; i < k; i++){
-			if(left <0 ){
-				res[i] = array[right++];
-			}else if(right >= array.length){
-				res[i] = array[left--];
-			}else if(Math.abs(array[left] -target) < Math.abs(array[right] - target)){
-				res[i] = array[left --];
-			}else res[i] = array[right++];
-		}
-	}
-	
-	public static int cloest(int[] array, int target){
-		int left = 0;
-		int right = array.length - 1;
 		while(left < right -1){
 			int mid = left + (right - left) / 2;
 			if(array[mid] == target) {
-				return mid;
+				index = mid;
 			}
 			else if(array[mid] < target){
 				left = mid;
@@ -35,6 +28,20 @@ public class kClosest {
 				right = mid;
 			}
 		}
+		
+		
+		for(int i = 0; i < k; i++){
+			if(left < 0){
+				res[i] = array[right++];
+			}else if(right >= array.length){
+				res[i] = array[left--];
+			}else if(Math.abs(array[left] -target) < Math.abs(array[right] - target)){
+				res[i] = array[left --];
+			}else res[i] = array[right++];
+		}
+		return res;
 	}
+	
+
 	
 }
